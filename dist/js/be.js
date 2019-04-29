e37168df3d1a70228698f0c1a0c88f2cca06cccb
unTsar <35622885+unTsar@users.noreply.github.com>
2019-04-29 22:24:05 +0300

    Finished Correstions 1.3

--- dist/js/be.js
+++ dist/js/be.js
@@ -5,11 +5,11 @@ $(document).ready(function(){
 
     });
     var hammer = new Hammer(document.querySelector('#carousel1'));
-    hammer.get("pan");
-    hammer.on("panleft", function(){
+    hammer.get("swipe");
+    hammer.on("swipeleft", function(){
         $('#carousel1').carousel("next");
     });
-    hammer.on("panright", function(){
+    hammer.on("swiperight", function(){
         $('#carousel1').carousel("prev");
     });
     $('.prevSlick').click(function() {
@@ -28,11 +28,11 @@ $(document).ready(function(){
       
     });
     var hammer = new Hammer(document.querySelector('#carousel2'));
-    hammer.get("pan");
-    hammer.on("panleft", function(){
+    hammer.get("swipe");
+    hammer.on("swipeleft", function(){
         $('#carousel2').carousel("next");
     });
-    hammer.on("panright", function(){
+    hammer.on("swiperight", function(){
         $('#carousel2').carousel("prev");
     });
     $('.prevCsl').click(function() {
@@ -44,26 +44,213 @@ $(document).ready(function(){
     $('#carousel2').on('slide.bs.carousel', function (e) {
       $("#counter2").text('0' + (e.to + 1));
     })
-    // Carousel 3
+    // Carousel 3 Purpose
     $('#carousel3').carousel({
       interval: false,
-      
+      wrap: false
     });
     var hammer = new Hammer(document.querySelector('#carousel3'));
-    hammer.get("pan");
-    hammer.on("panleft", function(){
+    hammer.get("swipe");
+    hammer.on("swipeleft", function(){
         $('#carousel3').carousel("next");
     });
-    hammer.on("panright", function(){
+    hammer.on("swiperight", function(){
         $('#carousel3').carousel("prev");
     });
+
     $('.prevCsl2').click(function() {
       $('#carousel3').carousel('prev');
     })
     $('.nextCsl2').click(function() {
       $('#carousel3').carousel('next');
     })
+    $('#PurposeCslArrowPrev').click(function() {
+      $('#carousel3').carousel('prev');
+    })
+    $('#PurposeCslArrowNext').click(function() {
+      $('#carousel3').carousel('next');
+    })
 
+    $('#PurposeCslArrowPrev').hide();
+
+    $('#carousel3').on('slide.bs.carousel', function (e) {
+      if (e.to == 0) {
+        $('#PurposeCslArrowPrev').hide();
+      }
+      if (e.to != 0) {
+        $('#PurposeCslArrowPrev').show();
+      }
+      if (e.to == 3) {
+        $('#PurposeCslArrowNext').hide();
+      }
+      if (e.to != 3) {
+        $('#PurposeCslArrowNext').show();
+      }
+    })
+
+    // Section Purpose Show|Hide
+    $('#PurposeBtnShow1').click(function() {
+      $(this).hide();
+      $('#textHide1').slideToggle(250);
+      if ($('#textHide2').css('display') != 'none') {
+        $('#PurposeBtnShow2').show();
+        $('#textHide2').slideToggle(250);
+      }
+      if ($('#textHide3').css('display') != 'none') {
+        $('#PurposeBtnShow3').show();
+        $('#textHide3').slideToggle(250);
+      }
+      if ($('#textHide4').css('display') != 'none') {
+        $('#PurposeBtnShow4').show();
+        $('#textHide4').slideToggle(250);
+      }
+    })
+    $('#PurposeBtnHide1').click(function() {
+      $('#PurposeBtnShow1').show();
+      $('#textHide1').slideToggle(250);
+    })
+    $('#PurposeBtnShow2').click(function() {
+      $(this).hide();
+      $('#textHide2').slideToggle(250);
+      if ($('#textHide1').css('display') != 'none') {
+        $('#PurposeBtnShow1').show();
+        $('#textHide1').slideToggle(250);
+      }
+      if ($('#textHide3').css('display') != 'none') {
+        $('#PurposeBtnShow3').show();
+        $('#textHide3').slideToggle(250);
+      }
+      if ($('#textHide4').css('display') != 'none') {
+        $('#PurposeBtnShow4').show();
+        $('#textHide4').slideToggle(250);
+      }
+    })
+    $('#PurposeBtnHide2').click(function() {
+      $('#PurposeBtnShow2').show();
+      $('#textHide2').slideToggle(250);
+    })
+    $('#PurposeBtnShow3').click(function() {
+      $(this).hide();
+      $('#textHide3').slideToggle(250);
+      if ($('#textHide2').css('display') != 'none') {
+        $('#PurposeBtnShow2').show();
+        $('#textHide2').slideToggle(250);
+      }
+      if ($('#textHide1').css('display') != 'none') {
+        $('#PurposeBtnShow1').show();
+        $('#textHide1').slideToggle(250);
+      }
+      if ($('#textHide4').css('display') != 'none') {
+        $('#PurposeBtnShow4').show();
+        $('#textHide4').slideToggle(250);
+      }
+    })
+    $('#PurposeBtnHide3').click(function() {
+      $('#PurposeBtnShow3').show();
+      $('#textHide3').slideToggle(250);
+    })
+    $('#PurposeBtnShow4').click(function() {
+      $(this).hide();
+      $('#textHide4').slideToggle(250);
+      if ($('#textHide2').css('display') != 'none') {
+        $('#PurposeBtnShow2').show();
+        $('#textHide2').slideToggle(250);
+      }
+      if ($('#textHide3').css('display') != 'none') {
+        $('#PurposeBtnShow3').show();
+        $('#textHide3').slideToggle(250);
+      }
+      if ($('#textHide1').css('display') != 'none') {
+        $('#PurposeBtnShow1').show();
+        $('#textHide1').slideToggle(250);
+      }
+    })
+    $('#PurposeBtnHide4').click(function() {
+      $('#PurposeBtnShow4').show();
+      $('#textHide4').slideToggle(250);
+    })
+
+    // mobile
+    $('#PurposeBtnShow1m').click(function() {
+      $(this).hide();
+      $('#textHide1m').slideToggle(250);
+      if ($('#textHide2m').css('display') != 'none') {
+        $('#PurposeBtnShow2m').show();
+        $('#textHide2m').slideToggle(250);
+      }
+      if ($('#textHide3m').css('display') != 'none') {
+        $('#PurposeBtnShow3m').show();
+        $('#textHide3m').slideToggle(250);
+      }
+      if ($('#textHide4m').css('display') != 'none') {
+        $('#PurposeBtnShow4m').show();
+        $('#textHide4m').slideToggle(250);
+      }
+    })
+    $('#PurposeBtnHide1m').click(function() {
+      $('#PurposeBtnShow1m').show();
+      $('#textHide1m').slideToggle(250);
+    })
+    $('#PurposeBtnShow2m').click(function() {
+      $(this).hide();
+      $('#textHide2m').slideToggle(250);
+      if ($('#textHide1m').css('display') != 'none') {
+        $('#PurposeBtnShow1m').show();
+        $('#textHide1m').slideToggle(250);
+      }
+      if ($('#textHide3m').css('display') != 'none') {
+        $('#PurposeBtnShow3m').show();
+        $('#textHide3m').slideToggle(250);
+      }
+      if ($('#textHide4m').css('display') != 'none') {
+        $('#PurposeBtnShow4m').show();
+        $('#textHide4m').slideToggle(250);
+      }
+    })
+    $('#PurposeBtnHide2m').click(function() {
+      $('#PurposeBtnShow2m').show();
+      $('#textHide2m').slideToggle(250);
+    })
+    $('#PurposeBtnShow3m').click(function() {
+      $(this).hide();
+      $('#textHide3m').slideToggle(250);
+      if ($('#textHide2m').css('display') != 'none') {
+        $('#PurposeBtnShow2m').show();
+        $('#textHide2m').slideToggle(250);
+      }
+      if ($('#textHide1m').css('display') != 'none') {
+        $('#PurposeBtnShow1m').show();
+        $('#textHide1m').slideToggle(250);
+      }
+      if ($('#textHide4m').css('display') != 'none') {
+        $('#PurposeBtnShow4m').show();
+        $('#textHide4m').slideToggle(250);
+      }
+    })
+    $('#PurposeBtnHide3m').click(function() {
+      $('#PurposeBtnShow3m').show();
+      $('#textHide3m').slideToggle(250);
+    })
+    $('#PurposeBtnShow4m').click(function() {
+      $(this).hide();
+      $('#textHide4m').slideToggle(250);
+      if ($('#textHide2m').css('display') != 'none') {
+        $('#PurposeBtnShow2m').show();
+        $('#textHide2m').slideToggle(250);
+      }
+      if ($('#textHide3m').css('display') != 'none') {
+        $('#PurposeBtnShow3m').show();
+        $('#textHide3m').slideToggle(250);
+      }
+      if ($('#textHide1m').css('display') != 'none') {
+        $('#PurposeBtnShow1m').show();
+        $('#textHide1m').slideToggle(250);
+      }
+    })
+    $('#PurposeBtnHide4m').click(function() {
+      $('#PurposeBtnShow4m').show();
+      $('#textHide4m').slideToggle(250);
+    })
 
   	// Fade bg content
   	$('#content-bg1-button').click(function() {
