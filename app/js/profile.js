f00b0b719d2c3304c6c1ae54bbb95535e5248f12
unTsar <35622885+unTsar@users.noreply.github.com>
2019-06-30 00:21:58 +0300

    Update profile

--- app/js/profile.js
+++ app/js/profile.js
@@ -1,32 +1,14 @@
 $(document).ready(function(){
+
 	$('.profile-buttonClose').click(function() {
 		$(this).css('display','none');
 		$(this).siblings('.profile-buttonOpen').css('display','flex');
+		$(this).parents('.head').siblings('.hidden').slideToggle(250);
 	});
 	$('.profile-buttonOpen').click(function() {
 		$(this).css('display','none');
 		$(this).siblings('.profile-buttonClose').css('display','flex');
-	});
-	$('.profile-button1').click(function() {
-		$('#profile-hidden1').slideToggle(250);
-	});
-	$('.profile-button2').click(function() {
-		$('#profile-hidden2').slideToggle(250);
-	});
-	$('.profile-button3').click(function() {
-		$('#profile-hidden3').slideToggle(250);
-	});
-	$('.profile-button4').click(function() {
-		$('#profile-hidden4').slideToggle(250);
-	});
-	$('.profile-button5').click(function() {
-		$('#profile-hidden5').slideToggle(250);
-	});
-	$('.profile-button6').click(function() {
-		$('#profile-hidden6').slideToggle(250);
-	});
-	$('.profile-button7').click(function() {
-		$('#profile-hidden7').slideToggle(250);
+		$(this).parents('.head').siblings('.hidden').slideToggle(250);
 	});
 
 	// Tooltip
