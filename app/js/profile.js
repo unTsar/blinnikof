52d585582b40eca948e2bcf1c11279d0dd17125c
unTsar <35622885+unTsar@users.noreply.github.com>
2019-07-02 21:14:43 +0300

    Final Dekstop profile

--- app/js/profile.js
+++ app/js/profile.js
@@ -1,35 +1,55 @@
 $(document).ready(function(){
 
-	$('.profile-buttonClose').click(function() {
-		$(this).css('display','none');
-		$(this).siblings('.profile-buttonOpen').css('display','flex');
-		$(this).parents('.head').siblings('.hidden').slideToggle(250);
-	});
 	$('.profile-buttonOpen').click(function() {
 		$(this).css('display','none');
 		$(this).siblings('.profile-buttonClose').css('display','flex');
 		$(this).parents('.head').siblings('.hidden').slideToggle(250);
 	});
+	$('.profile-buttonClose').click(function() {
+		$(this).css('display','none');
+		$(this).siblings('.profile-buttonOpen').css('display','flex');
+		$(this).parents('.head').siblings('.hidden').slideToggle(250);
 
+		$(this).parents('.head').siblings('.hidden').find('.hidden').css('display', 'none');
+		$(this).parents('.head').siblings('.hidden').find('.profile-buttonOpen').css('display','flex');
+		$(this).parents('.head').siblings('.hidden').find('.profile-buttonClose').css('display','none');
+	});
 	// Tooltip
 	$(function () {
 		$('[data-toggle="tooltip"]').tooltip()
 	})
 
-	// ScrollTo
-	$('#scrollTo-btn1').click(function(){
-      $.scrollTo('.personInf', 500);
-    });
-    $('#scrollTo-btn2').click(function(){
-      $.scrollTo('.orders', 500);
-    });
-    $('#scrollTo-btn3').click(function(){
-      $.scrollTo('.sTicket', 500);
-    });
-    $('#scrollTo-btn4').click(function(){
-      $.scrollTo('.stats', 500);
-    });
-    $('#scrollTo-btn5').click(function(){
-      $.scrollTo('.appNutrition', 500);
-    });
+	$('.orders-buttonUnpaid').click(function() {
+
+		$(this).addClass('active');
+		$(this).siblings('.orders-buttonPaid').removeClass('active');
+
+		$(this).parents('.buttons').siblings('.orders-listPaid').slideUp(175);
+		$(this).parents('.buttons').siblings('.orders-listUnpaid').slideDown(175);
+	})
+	$('.orders-buttonPaid').click(function() {
+
+		$(this).addClass('active');
+		$(this).siblings('.orders-buttonUnpaid').removeClass('active');
+
+		$(this).parents('.buttons').siblings('.orders-listUnpaid').slideUp(175);
+		$(this).parents('.buttons').siblings('.orders-listPaid').slideDown(175);
+	})
+
+	$('#profile-buttonUnpaid-sTicket').click(function() {
+		$('.schedule-Unpaid').addClass('hidden');
+		if ($('.schedule-Unpaid').siblings('.head').children('.profile-buttonOpen').css('display') == 'none') {
+			$('.schedule-Unpaid').fadeIn();
+		}
+		$('.schedule-Paid').removeClass('hidden');
+		$('.schedule-Paid').hide();
+	})
+	$('#profile-buttonPaid-sTicket').click(function() {
+		$('.schedule-Paid').addClass('hidden');
+		if ($('.schedule-Paid').siblings('.head').children('.profile-buttonOpen').css('display') == 'none') {
+			$('.schedule-Paid').fadeIn();
+		}
+		$('.schedule-Unpaid').removeClass('hidden');
+		$('.schedule-Unpaid').hide();
+	})
 });
